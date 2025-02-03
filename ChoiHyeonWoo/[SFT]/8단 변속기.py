4cb72d06562969b11712cd0d876239cd90105b4d
Hyeon_Wooooo <84663334+po2955@users.noreply.github.com>
2025-02-03 20:26:03 +0900

    Delete ChoiHyeonWoo/[SFT]/8단 변속기.py

--- ChoiHyeonWoo/[SFT]/8단 변속기.py
+++
@@ -1,9 +0,0 @@
-gear = list(map(int, input().split()))
-check_1 = [1, 2, 3, 4, 5, 6, 7, 8]
-check_2 = [8, 7, 6, 5, 4 ,3, 2, 1]
-if gear == check_1:
-    print('ascending')
-elif gear == check_2:
-    print('descending')
-else:
-    print('mixed')
